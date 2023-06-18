3ef294cc2157e70ae47599d40986b6b7c5b93a6a
boooiil <boooiil@outlook.com>
2023-06-18 01:12:22 -0400

    remove redundant checks

--- anime_rename.js
+++ anime_rename.js
@@ -13,8 +13,10 @@ function nextLevel(path) {
     console.log("Next Level", path)
 
     let season;
+    let series_name;
 
     if (/(?<=Season |Season)([0-9][0-9]|[1-9])(?=)/i.test(path)) season = path.match(/(?<=Season |Season)([0-9][0-9]|[1-9])(?=)/i)[0]
+    if (/(?<=\/)(.*)(?= Season)/i.test(path)) series_name = path.match(/(?<=\/)(.*)(?= Season)/i)[0]
 
     season = season < 10 ? "0" + season : season
 
@@ -36,9 +38,10 @@ function nextLevel(path) {
         episode.ext = null
         episode.file = file
         episode.n_file = null
-        episode.show = null
+        episode.show = series_name
         episode.number = /[0-9][0-9][0-9]|[0-9][0-9]/.test(file) ? file.match(/[0-9][0-9][0-9]|[0-9][0-9]/)[0] : null
         episode.episode = null
+        episode.season = season
 
         if (/s[0-9]+e[0-9]+|s[0-9]+.e[0-9]+/i.test(file)) {
 
@@ -62,53 +65,16 @@ function nextLevel(path) {
 
         episode.n_file = file;
         episode.ext = file.match(/\.mp4|\.mkv|\.mov|\.avi|\.m4p/)[0]
+        episode.n_file = file.replace(episode.ext, "")
+        episode.n_file = file.replace(/\./g, " ");
+
+        console.log(episode.n_file)
 
         console.log(`[Next Level] [${index}] Looking at file: ${file} with extension ${episode.ext}`)
 
         if (!episode.ext) return;
 
-        if (/\[.*?\]/.test(file)) {
-
-            console.log(`[Next Level] [${index}] File ${file} tested positive with match ${file.match(/\[.*?\]/)[0]}`)
-
-            episode.n_file = episode.n_file.replace(/\[.*?\]/, "")
-
-        }
-
-        if (/- [0-9][0-9]|[0-9][0-9] -/.test(episode.n_file)) {
-
-            episode.n_file = episode.n_file.replace(/(?<=- [0-9][0-9]|[0-9][0-9] -)(.*)(?=$)/, "")
-            episode.n_file = episode.n_file.replace(episode.n_file.match(/- [0-9][0-9]|[0-9][0-9] -/)[0], "- " + episode.n_file.match(/[0-9][0-9]/)[0])
-            episode.n_file = episode.n_file.replace(/episode /i, "")
-            episode.n_file = episode.n_file.replace(/\./, " ")
-
-
-        }
-
-        else if (/- [0-9][0-9][0-9]|[0-9][0-9][0-9] -/.test(episode.n_file)) {
-
-            episode.n_file = episode.n_file.replace(/(?<=- [0-9][0-9][0-9]|[0-9][0-9][0-9] -)(.*)(?=$)/, "")
-            episode.n_file = episode.n_file.replace(episode.n_file.match(/- [0-9][0-9][0-9]|[0-9][0-9][0-9] -/)[0], "- " + episode.n_file.match(/[0-9][0-9][0-9]/)[0])
-            episode.n_file = episode.n_file.replace(/episode /i, "")
-            episode.n_file = episode.n_file.replace(/\./, " ")
-
-        }
-
-        else if (/S[0-9]+E[0-9]+/.test(episode.n_file)) {
-
-            episode.n_file = episode.n_file.replace(/(?<=- S[0-9]+E[0-9]+|S[0-9]+E[0-9]+ -)(.*)(?=$)/, "")
-            episode.n_file = episode.n_file.replace(episode.n_file.match(/- S[0-9]+E[0-9]+|S[0-9]+E[0-9]+ -/)[0], "- " + episode.n_file.match(/S[0-9]+E[0-9]+/)[0])
-            episode.n_file = episode.n_file.replace(/episode /i, "")
-            episode.n_file = episode.n_file.replace(/\./, " ")
-
-        }
-
-        else throw "Unable to determine epsidoe format."
-        
-
-        episode.show = episode.n_file.match(/(?<=)(.*)(?= - [0-9][0-9]+|- S[0-9]+E[0-9]+)/)[0].trim()
-
-        episode.n_file = `${episode.show} - S${season}E${episode.episode}${episode.ext}`
+        episode.n_file = `${episode.show} - s${season}e${episode.episode}${episode.ext}`
         episode.n_path = episode.folder_path + "/" + episode.n_file
 
         fs.renameSync(episode.file_path, episode.n_path)
