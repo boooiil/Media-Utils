7318c392df6a8dd84b7a51dc4188996b8784e706
boooiil <boooiil@outlook.com>
2023-06-18 03:56:08 -0400

    hide banner

--- Convert_MKV.bat
+++ Convert_MKV.bat
@@ -1,12 +1,12 @@
 for %%a in ("*.avi") DO (
-	ffmpeg.exe -fflags +genpts -hwaccel cuda -i "%%a" -c:v copy -c:a copy -n "%%~na.mkv"
+	ffmpeg.exe -hide_banner -fflags +genpts -hwaccel cuda -i "%%a" -c:v copy -c:a copy -n "%%~na.mkv"
 	rem del "%%a"
 )
 for %%a in ("*.mp4") DO (
-	ffmpeg.exe -hwaccel cuda -i "%%a" -c:v copy -c:a copy -n "%%~na.mkv"
+	ffmpeg.exe -hide_banner -hwaccel cuda -i "%%a" -c:v copy -c:a copy -n "%%~na.mkv"
 	rem del "%%a"
 )
 for %%a in ("*.divx") DO (
-	ffmpeg.exe -fflags +genpts -hwaccel cuda -i "%%a" -c:v copy -c:a copy -n "%%~na.mkv"
+	ffmpeg.exe -hide_banner -fflags +genpts -hwaccel cuda -i "%%a" -c:v copy -c:a copy -n "%%~na.mkv"
 	rem del "%%a"
 )
